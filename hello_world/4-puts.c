b7a1e9d9531287fa3963959afea63674d492b367
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-02-15 10:41:16 -0800

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void){
-	//gcc puts("Programming is like building a multilingual puzzle") 
-	retrun 0;
+	//gcc puts("/"Programming is like building a multilingual puzzle"); 
+	retrun (0);
 }

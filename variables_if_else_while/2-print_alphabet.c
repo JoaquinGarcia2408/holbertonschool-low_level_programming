5e4b6c5fa96446091aa0bdcf71ddbeacf427cbda
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-02-16 10:06:00 -0800

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	char *abc = "abcdefghijklmnopqrstuvwxyz";
+	int letra;
+
+	for (letra = 0; letra <= 25; letra++)
+		putchar(abc[letra]);
+	return (0);
+}

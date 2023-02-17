4d8c7f7de1db0bbad39abf5ed92180206a02c09f
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-02-17 04:48:20 -0800

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int num = '0';
+		for (num = '0'; num <= '9'; num++)
+			putchar(num);
+	putchar('\n');
+	return (0);
+}

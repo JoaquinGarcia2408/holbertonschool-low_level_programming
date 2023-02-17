e86fbaf470cee4d09b08481d377fd6e687eccbc7
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-02-17 07:21:00 -0800

    0-putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   * Return: Always 0 (Success)
+ *    **/
+int main(void)
+{
+	printf("_putchar");
+	putchar('\n');
+	return (0);
+}

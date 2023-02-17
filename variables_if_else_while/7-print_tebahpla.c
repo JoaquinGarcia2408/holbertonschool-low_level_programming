fe34513754a4bc98814731f4ab19126fac675aba
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-02-17 05:21:00 -0800

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * return: always 0 (Success)
+ **/
+int main(void)
+{
+	char *cba = "abcdefghijklmnopqrstuvwxyz";
+	int lector;
+
+	for (lector = 25; lector > 0; lector--)
+		putchar(cba[lector]);
+	putchar('\n');
+	return (0);
+}

974ce9f0bf974a5022d8e1d0bab86dd21208ec3d
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-02-19 10:42:09 -0800

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *  *  * jack_bauer - Prints every minute and hour from 00:00 to 23:59
+ *   *   * @: The time.
+ *    *     * Return: Value of the last digit.
+ *    **/
+void jack_bauer(void)
+{
+	int min;
+	int hor;
+	for (hor = 0; hor <= 23; hor++)
+	{
+	for (min = 0; min <= 59; min++)
+	{
+		_putchar((hor / 10) + '0');
+		_putchar((hor % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar((min % 10) + '0');
+		_putchar('\n');
+	}
+	}
+}

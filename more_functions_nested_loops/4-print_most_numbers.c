cb6e037db376b8a9e1380a7e5f2175861365ead9
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-02-23 05:08:12 -0800

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,7 +8,7 @@ void print_most_numbers(void)
 	char *num = "01356789";
 	int letra;
 
-	for (letra = 0; letra <= 8; letra++)
+	for (letra = 0; letra <= 9; letra++)
 		putchar(num[letra]);
 	putchar('\n');
 }

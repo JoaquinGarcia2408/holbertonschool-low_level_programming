7dfc9ebed2327613f0ab6523ed96501c9b0dd1fd
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-02-23 05:00:30 -0800

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_most_numbers - check the code.
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	char *num = "01356789";
+	int letra;
+
+	for (letra = 0; letra <= 8; letra++)
+		putchar(num[letra]);
+	putchar('\n');
+}

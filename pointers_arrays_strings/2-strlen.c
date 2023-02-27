b46c74f9a7182a8ef1ae472e2fdc295e229bbe6e
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-02-27 09:14:06 -0800

    2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,11 +2,13 @@
 /**
  * _strlen - function that returns the length of a string.
  * @s: variable
+ * Return: Always 0.
  **/
 int _strlen(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++);
+	for (i = 0; s[i] != '\0'; i++)
+		;
 	return (i);
 }

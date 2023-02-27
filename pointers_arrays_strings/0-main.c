c0da3eb5272cc4b67ea14a2be3e494687a132682
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-02-27 07:00:20 -0800

    0-putchar.c

--- pointers_arrays_strings/0-main.c
+++ pointers_arrays_strings/0-main.c
@@ -1,18 +1,19 @@
-#include "main.h"
 #include <stdio.h>
 
+void reset_to_98(int *);
+
 /**
- * main - check the code 
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+	n = 402;
+	printf("%d\n", n);
+	reset_to_98(&n);
+	printf("%d\n", n);
+	return (0);
 }

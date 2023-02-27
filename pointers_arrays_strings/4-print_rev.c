2e5de9d12ffd70411a75892ba2d97eddb5ff6bae
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-02-27 10:28:32 -0800

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: cadena
+ * Return: Always 0.
+ **/
+void print_rev(char *s)
+{
+	int lector;
+        int i;
+
+        for (i = 0; s[i] != '\0'; i++)
+                ;
+        for (lector = i; lector >= 0; lector--)
+	{
+		_putchar(s[lector]);
+	}
+	_putchar('\n');
+}

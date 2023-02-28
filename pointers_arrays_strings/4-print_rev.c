3dc0fd93fb10f6b893509f5bfa04937f89260bf5
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-02-28 04:58:01 -0800

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,9 +9,10 @@ void print_rev(char *s)
 	int lector, i;
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
-	}
+		;
 	for (lector = i; lector >= 0; lector--)
-			_putchar(s[lector]);
+	{
+		_putchar(s[lector]);
+	}
 	_putchar('\n');
 }

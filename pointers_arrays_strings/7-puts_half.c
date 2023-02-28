c013f377c4c9426967071a1e65b69b1f418879bd
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-02-28 10:06:29 -0800

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ * Return: Always 0.
+ **/
+void puts_half(char *str)
+{
+	int lector = 0, tam = 0;
+
+	while (str[lector])
+	{
+		tam++;
+		lector++;
+	}
+	for (lector = 0; lector <= tam; lector++)
+	{
+		if (tam % 2 == 0 && lector >= tam / 2)
+			_putchar(str[lector]);
+		else if (lector - 1 >= tam / 2)
+			_putchar(str[lector]);
+	}
+	_putchar('\n');
+}

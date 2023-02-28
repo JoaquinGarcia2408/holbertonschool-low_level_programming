cb51edc0ff17c21c285aa5515056d3478dbc62b6
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-02-28 06:19:09 -0800

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string
+ *Return: Always 0.
+ **/
+void rev_string(char *s)
+{
+	int tam, cont;
+	char intercambio;
+
+        for (tam = 0; s[tam] != '\0'; tam++) /** me da  el tamaño de la cadena **/
+		;
+	for (cont = 0; cont < tam/2; cont++) /** intercamcia los caracteres s[cont] y s[tam-1-i] **/ 
+	{
+		intercambio = s[cont];
+		s[cont] = s[tam-1-cont];
+		s[tam-1-cont] = intercambio;
+	}
+}

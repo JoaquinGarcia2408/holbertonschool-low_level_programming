9266083c03e12f85e548cf1dee35fbeef8e11b80
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-05 10:48:12 -0800

    5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: string 1.
+ * @needle: string 2.
+ * Return: 0
+ **/
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *h = haystack;
+		char *n = needle;
+		while (*h == *n && *n != '\0')
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return (NULL);
+}

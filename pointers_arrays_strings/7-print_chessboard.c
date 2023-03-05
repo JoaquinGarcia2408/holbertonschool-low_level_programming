95fe9cee9415a18e08d429c2bae7ed608190e3cb
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-05 11:12:08 -0800

    7-print_chessboard.c

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard
+ * @a: The chessboard to be printed
+ **/
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j]);
+		}
+		putchar('\n');
+	}
+}

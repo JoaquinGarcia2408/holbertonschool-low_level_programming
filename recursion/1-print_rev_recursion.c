45459b8fc682cfdbe1d012dbe605de9d3221a6a8
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-06 08:52:58 -0800

    1-print_rev_recursion.c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev_recursion - prints a string in reverse.
+ * @s: string
+ * Return: Always 0.
+ **/
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion (s + 1);
+		_putchar(*s);
+	}
+}

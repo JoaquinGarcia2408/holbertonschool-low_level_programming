f7573f3c55e5f9183251748170aca147debe9a31
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-06 06:27:49 -0800

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -6,12 +6,12 @@
  **/
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
 	if (*s != '\0')
 	{
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	else
+	else if (*s == '\0')
 	{
 		_putchar('\n');
 	}

91e8a35eb02f4fa0805dca1e3b5c39c3cc3389c4
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-06 05:59:40 -0800

    0-puts_recursion.c

---
+++ recursion/0-main.c
@@ -0,0 +1,12 @@
+void _puts_recursion(char *);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_puts_recursion("First, solve the problem. Then, write the code");
+	return (0);
+}

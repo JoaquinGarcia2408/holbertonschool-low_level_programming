cd1efaba0276613b2306beb739e4272af60ce7b2
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-07 05:43:33 -0800

    4-pow_recursion.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: number
+ * @y: power
+ * Return: always 0
+ **/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else if (y == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (x *= _pow_recursion(x, y - 1));
+	}
+}

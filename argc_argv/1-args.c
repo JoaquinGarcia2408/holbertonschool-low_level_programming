66098c4df9e577cf5682f85b475778eb9dfbc137
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-09 10:34:00 -0800

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - print number of arguents
+ * @argc: variable
+ * @argv: variable
+ * Return: 0
+ **/
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}

4083f1a3093dcfc772a09373dc280284feed92b1
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-09 09:56:58 -0800

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - print name
+ * @argc: cantidad de palabras
+ * @argv: pointer strings
+ * Retur: 0.
+ **/
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

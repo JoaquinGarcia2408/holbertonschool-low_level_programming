0abca65d2bf4df8fe787ad63801412786a04d9a0
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-13 08:41:35 -0700

    0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * create_array - variable
+ * @size: tamaño
+ * @c: caracter
+ * Return: always 0.
+ **/
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i, n;
+
+	*ptr = malloc(n * sizeof(char);
+
+	if (ptr == NULL)
+	{
+		exit(0);
+	}
+	else if (size != 0)
+	{
+		for (i = 0; i < n; i++)
+			ptr[i] = c
+		return (ptr);
+	}
+	else
+	{
+		return (NULL);
+	}
+}

9c15ab0fdf9a7b372a2c7b4d4fe637965279f3b8
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-15 07:42:24 -0700

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -33,17 +33,14 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	else
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		for (i = 0; s1[i] != '\0'; i++)
-		{
-			p[i] = s1[i];
-			t1++;
-		}
-		for (i = 0; s2[i]; i++)
-		{
-			p[t1++] = s2[i];
-		}
+		p[i] = s1[i];
+		t1++;
+	}
+	for (i = 0; s2[i]; i++)
+	{
+		p[t1++] = s2[i];
 	}
 	return (p);
 }

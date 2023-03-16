7a070722d5bb9f2ce6c9b4dc7e472e12b058dd7b
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-16 10:11:43 -0700

    0-malloc_checked.c

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,5 +1,6 @@
 #include <stdlib>
 #include <string.h>
+#include <stdlib>
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);

5d26b64ca9a311637c90a1a8b5ea15f585e5ee9b
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-16 11:06:30 -0700

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,14 +1,35 @@
 #include "main.h"
 /**
+ * _memset - function that fills memory with a constant byte
+ * @s: area pointed;
+ * @b: constant byte;
+ * @n: the number of bytes;
+ * Return: s;
+ **/
+void *_memset(void *s, char b, unsigned int n)
+{
+	unsigned int i;
+	unsigned char *ptr = s;
+
+	for (i = 0; i < n; i++)
+		ptr[i] = (unsigned char)b;
+	return (ptr);
+}
+/**
+ * _calloc - prototype
+ * @nmemb: numero
+ * @size: tamaño
+ * Return: always 0
  **/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *p;
+	void *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(sizeof(size));
+	p = malloc(size * nmemb);
 	if (p == NULL)
 		return (NULL);
+	_memset(p, 0, (nmemb * size));
 	return (p);
 }

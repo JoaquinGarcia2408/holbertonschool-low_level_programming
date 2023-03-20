798f21795047ed6bc4b3ff54cfff364b1a66c5e8
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-20 07:25:41 -0700

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#ifndef INIT_DOG
+#define INIT_DOG
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - variable
+ * @d: dog
+ * @name: nombre del perro
+ * @age: edad del perro
+ * @owner: dueño del perro
+ * Return: void
+ **/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d-> name = name;
+	d-> age = age;
+	d-> owner = owner;	
+}
+#endif

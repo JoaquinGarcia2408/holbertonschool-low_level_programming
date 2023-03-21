da1891da2137503387bf86127f574affcf059c82
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-21 08:54:21 -0700

    2-int_index.c

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#ifndef INT_INDEX
+#define INT_INDEX
+#include "function_pointers.h"
+/**
+ * int_index - funcion
+ * @array: array
+ * @size: tamaño
+ * @cmp: algo
+ * Return: always 0
+ **/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (array != NULL && cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				return(i);
+			}
+		}
+	}
+	return (0);
+}
+#endif

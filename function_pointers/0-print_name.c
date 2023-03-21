61f573b9eb07584b085f6c7b8994cc58db07c872
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-21 06:19:05 -0700

    0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - puntero permite almacenar la dirección de una variable
+ * @name: nombre
+ * @f: paramether
+ * Return: 0
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}

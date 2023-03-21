8416c912f5a74bfae5c50393b5e2b8598397f337
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-21 06:22:58 -0700

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#ifndef PRINT_POINTER
+#define PRINT_POINTER
 #include "function_pointers.h"
 /**
  * print_name - puntero permite almacenar la dirección de una variable
@@ -10,3 +12,4 @@ void print_name(char *name, void (*f)(char *))
 	if (name != NULL && f != NULL)
 		f(name);
 }
+#endif

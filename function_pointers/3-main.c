82b329ee6e5d91f0f30a9920a225d37caca9f2d4
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-22 10:13:59 -0700

    calculator

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,7 +1,10 @@
 #include "3-calc.h"
 /**
+ * main - funcion
+ * @argc: number of arguments
+ * @argv: arrat of arguments
  **/
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int a, b;
 	int (*aux)(int, int);
@@ -17,9 +20,9 @@ int main (int argc, char *argv[])
 	b = atoi(argv[3]);
 	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
 	{
-		printf("Error/n");
+		printf("Error\n");
 		exit(100);
 	}
-	printf("%d/n", aux(a, b));
+	printf("%d\n", aux(a, b));
 	return (0);
 }

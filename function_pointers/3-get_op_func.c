48a4b3c215d53e85030dc3319b97a25ab69099cc
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-22 07:52:08 -0700

    calculator

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,21 @@
+int (*get_op_func(char *s))(int, int)
+{
+	 op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i = 0;
+
+	while (ops[i].op)
+	{
+		if (ops[i].op == s)
+		{
+			return (ops[i].f)
+		}
+		i++;
+	}
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - suma
+ * @a: numero
+ * @b: numero
+ * Return: always 0
+ **/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - resta
+ * @a: numero
+ * @b: numero
+ * Return: always 0
+ **/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplica
+ * @a: numero
+ * @b: numero
+ * Return: always 0
+ **/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divicion
+ * @a: numero
+ * @b: numero
+ * Return: always 0
+ **/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - resto
+ * @a: numero
+ * @b: numero
+ * Return: always 0
+ **/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

010fdb2a0f74e231cbca496ad202b28a39280492
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-04-05 07:42:49 -0700

    0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ **/
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}

abe4262fb229ad141db128c1fbb31da1fd666de3
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-04-12 10:26:11 -0700

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 06000);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file < 0)
 	{
 		return (-1);
